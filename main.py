a344eb436745aca241c700844411d15bfc3148b7
Max Kolle <maxkolle1@gmail.com>
2020-10-17 16:00:47 +0100

    Working pastebin clone base

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+import secrets 
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+def save_paste(paste):
+    paste_id = secrets.token_hex(16)
+    with open("paste/{}".format(paste_id), "w") as f:
+        f.write(paste)
+    return paste_id
+
+
+def get_paste(paste_id=None):
+    paste = None
+    try:
+        with open("paste/{}".format(paste_id), "r") as f:
+            paste = f.read()
+    except FileNotFoundError:
+        pass
+    finally:
+        return paste
+
+@app.route("/paste/", methods=["GET", "POST"])
+@app.route("/paste/<paste_id>")
+def paste(paste_id=None):
+    if request.method == "POST":
+        # Save the paste in the form to a file
+        paste_id = save_paste(request.form["paste"])
+        return redirect(url_for("paste") + paste_id)
+    paste = get_paste(paste_id)
+    return render_template("paste.html", paste_id=paste_id, paste=paste)
+    
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page_not_found.html"), 404
