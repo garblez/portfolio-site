60f8c18cf219eae809020ac69f80cdb0bb3fdbe8
Max Kolle <maxkolle1@gmail.com>
2020-10-19 14:43:14 +0100

    Add __init__

--- main.py
+++ main.py
@@ -45,3 +45,8 @@ def paste(paste_id=None):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("page_not_found.html"), 404
+
+
+
+if __name__ == "__main__":
+    app.run()
